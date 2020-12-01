d04130feb3b8ed1efc7c265f82e743e8819e9f36
yohanes_l <yohanes.lumentut@gmail.com>
2020-12-01 17:09:35 +0700

    Initial commit

---
+++ projects/ngx-history/src/lib/ngx-history.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { HistoryService } from './history.service';
+
+@NgModule({
+    declarations: [],
+    exports: [],
+    providers: [HistoryService]
+})
+export class NgxHistoryModule {}
--- projects/ngx-history/src/public-api.ts
+++ projects/ngx-history/src/public-api.ts
@@ -2,14 +2,5 @@
  * Public API Surface of ngx-history
  */
 
-import { NgModule } from '@angular/core';
-import { HistoryService } from './lib/history.service';
-
 export * from './lib/history.service';
-
-@NgModule({
-    declarations: [],
-    exports: [],
-    providers: [HistoryService]
-})
-export class NgxHistoryModule {}
+export * from './lib/ngx-history.module';
